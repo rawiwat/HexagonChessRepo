465629a28da1ff3cef3c19492b343232cdf7da9c
Peter <rawiwatpan@gmail.com>
2023-11-03 09:17:37 +0700

    commit 96 upgrade rook and queen to async

--- HexagonalChess/app/src/main/java/com/example/hexagonalchess/domain_layer/piecemove/rookMove.kt
+++ HexagonalChess/app/src/main/java/com/example/hexagonalchess/domain_layer/piecemove/rookMove.kt
@@ -6,17 +6,22 @@ import com.example.hexagonalchess.domain_layer.TileDirections
 import com.example.hexagonalchess.domain_layer.TileId
 import com.example.hexagonalchess.domain_layer.filterSameColor
 import com.example.hexagonalchess.domain_layer.getAllTileInDirection
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 
-fun rookMove(selectedTile: Tile, board: List<Tile>, boardType: BoardType):List<TileId?> {
-    val result = mutableListOf<TileId?>()
-    result.addAll(getAllTileInDirection(selectedTile, TileDirections.TOP, board, boardType))
-    result.addAll(getAllTileInDirection(selectedTile, TileDirections.UPPER_RIGHT, board, boardType))
-    result.addAll(getAllTileInDirection(selectedTile, TileDirections.UNDER_RIGHT, board, boardType))
-    result.addAll(getAllTileInDirection(selectedTile, TileDirections.BOTTOM, board, boardType))
-    result.addAll(getAllTileInDirection(selectedTile, TileDirections.UNDER_LEFT, board, boardType))
-    result.addAll(getAllTileInDirection(selectedTile, TileDirections.UPPER_LEFT, board, boardType))
+suspend fun rookMove(selectedTile: Tile, board: List<Tile>, boardType: BoardType):List<TileId?> = coroutineScope {
+    val deferredResult = mutableListOf<Deferred<List<TileId?>>>()
+    deferredResult.add(async { getAllTileInDirection(selectedTile, TileDirections.TOP, board, boardType) })
+    deferredResult.add(async { getAllTileInDirection(selectedTile, TileDirections.UPPER_RIGHT, board, boardType) })
+    deferredResult.add(async { getAllTileInDirection(selectedTile, TileDirections.UNDER_RIGHT, board, boardType) })
+    deferredResult.add(async { getAllTileInDirection(selectedTile, TileDirections.BOTTOM, board, boardType) })
+    deferredResult.add(async { getAllTileInDirection(selectedTile, TileDirections.UNDER_LEFT, board, boardType) })
+    deferredResult.add(async { getAllTileInDirection(selectedTile, TileDirections.UPPER_LEFT, board, boardType) })
 
+    val result = deferredResult.awaitAll().flatten().toMutableList()
     filterSameColor(selectedTile, result, board, boardType)
 
-    return result
+    return@coroutineScope result
 }
